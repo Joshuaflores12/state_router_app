ee844d84b84df226a3b0e648470cd681a51da87d
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-09-19 13:29:37 +0800

    react bootstrap upates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // lines 1-8 imports the js files inside the components folder
 // Browser rowrer is the root that binds all the routing in react making the user have easier navigation on the website
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route,Routes} from "react-router-dom";
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
--- src/Mynavbar.js
+++ src/Mynavbar.js
@@ -1,6 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import {Link} from "react-router-dom";
+
 
 
 function BasicExample() {
@@ -11,10 +13,10 @@ function BasicExample() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-            <Nav.Link href="/counter">Counter</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/counter">Counter</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
